645872c5dafef887bb337897554a0d664109dcc8
Patrick Jaap <jaap@wias-berlin.de>
2023-11-10 17:53:27 +0100

    Add adhesion with better smoothing function

--- examples/Example227_TwoMembranesWithAdhesion.jl
+++ examples/Example227_TwoMembranesWithAdhesion.jl
@@ -46,8 +46,8 @@ function χ(x,ϵ=0)
 	if x ≤ -ϵ
 		return 0
 	elseif x < 0
-		# return -x^3/(4ϵ^3) + 3x/(4ϵ)
-		return 1 + x/ϵ
+		return -2x^3/(ϵ^3) - 3x^2/(ϵ^2) + 1
+		# return 1 + x/ϵ
 	else
 		return 1
 	end
@@ -55,13 +55,13 @@ end
 
 
 # diff of heaviside function with smoothing at (-ϵ,0)
-function d_χ(x,ϵ=0)
+function d_χ(x,ϵ)
 
 	if x ≤ -ϵ
 		return 0
 	elseif x < 0
-		# return -3x^2/(4ϵ^3) + 3/(4ϵ)
-		return 1/ϵ
+		return -6x^2/ϵ^3 - 6x/ϵ^2
+		# return 1/ϵ
 	else
 		return 0
 	end
@@ -73,8 +73,7 @@ function make_kernel(f1, # membrane force 1
 				     f2, # membrane force 2
 					 θ,  # adhesion parameter
 	                 γ,  # penetration penalty
-	                 ϵ,  # smoothing parameter
-	                 boundary_offset)
+	                 ϵ)  # smoothing parameter
 
 
 	function nonlinear_kernel!(result, input, qpinfo )
@@ -89,11 +88,11 @@ function make_kernel(f1, # membrane force 1
 		# stash ... θ*( -d_χ(u2 - u1, ϵ) )
 
 		# first membrane force incl non-penetration
-		result[1]   =  γ*max(0, u1 - u2 )- f1 #  - θ*( -d_χ(u2 - u1, ϵ) )
+		result[1]   =  γ*max(0, u1 - u2 )- f1   - θ*( d_χ(u1 - u2, ϵ) )
 		result[2:3] = ∇u1												             
 
 		# second membrane force 
-		result[4]   =   -γ*max(0, u1 - u2 ) - f2 #	 + θ*( -d_χ(u2 - u1, ϵ) )     	     
+		result[4]   =   -γ*max(0, u1 - u2 ) - f2 + θ*( d_χ(u1 - u2, ϵ) )     	     
 		result[5:6] = ∇u2												             
 
 		return nothing
@@ -105,10 +104,10 @@ end
 
 ## custom sparsity pattern for the jacobians of the nonlinear_kernel (Symbolcs cannot handle conditional jumps)
 ## note: jacobians are defined row-wise
-rows = [1, 1, 1, 2, 3, 4, 4, 4, 5, 6, 7, 7, 7]
-cols = [1, 4, 7, 2, 3, 1, 4, 7, 5, 6, 1, 4, 7]
-vals = ones(Bool, length(cols))
-sparsity_pattern = sparse(rows,cols,vals)
+# rows = [1, 1, 1, 2, 3, 4, 4, 4, 5, 6, 7, 7, 7]
+# cols = [1, 4, 7, 2, 3, 1, 4, 7, 5, 6, 1, 4, 7]
+# vals = ones(Bool, length(cols))
+# sparsity_pattern = sparse(rows,cols,vals)
 
 
 
@@ -136,8 +135,8 @@ function main(;
 	assign_unknown!(PD, u2)
 	
 	@warn "sparse_jacobians = false for prototyping"
-	nonlinear_kernel! = make_kernel(f1,f2,θ,γ,ϵ,boundary_offset)
-	assign_operator!(PD, NonlinearOperator(nonlinear_kernel!, [id(u1), grad(u1), id(u2), grad(u2)]; bonus_quadorder=2, sparse_jacobians=false, kwargs...))
+	nonlinear_kernel! = make_kernel(f1,f2,θ,γ,ϵ)
+	assign_operator!(PD, NonlinearOperator(nonlinear_kernel!, [id(u1), grad(u1), id(u2), grad(u2)]; sparse_jacobians=false, bonus_quadorder=2, kwargs...))
 	assign_operator!(PD, HomogeneousBoundaryData(u1; regions = 1:4,  kwargs...))
 	assign_operator!(PD, HomogeneousBoundaryData(u2; regions = 1:4, value = boundary_offset,  kwargs...))
 
@@ -149,7 +148,7 @@ function main(;
 	interpolate!(sol[u2], (result,qp) -> ( result[1] = boundary_offset ) )
 
 	## solve
-	sol = solve(PD, FESs; init = sol, maxiterations=200, target_residual=1e-8, kwargs...)
+	sol = solve(PD, FESs; init = sol, maxiterations=100, target_residual=1e-8, kwargs...)
 
 	u_min = minimum(sol.entries)
 	u_max = maximum(sol.entries)
@@ -159,8 +158,6 @@ function main(;
     pl = GridVisualizer(; Plotter = Plotter, layout = (1,2), clear = true, size = (1000,500))
     scalarplot!(pl[1,1],xgrid, view(nodevalues(sol[u1]),1,:), title = "u1", flimits = (u_min, u_max) )
     scalarplot!(pl[1,2],xgrid, view(nodevalues(sol[u2]),1,:), title = "u2", flimits = (u_min, u_max) )
-
-	@show minimum(view(sol[u2])) maximum(view(sol[u1]))
 
 	# compute portion of contact
 	dofs_in_contact = 0
@@ -174,7 +171,7 @@ function main(;
 
 	@show portion_of_contact
 
-	@show minimum(abs.(view(sol[u1]) .- view(sol[u2])))
+	@show maximum(view(sol[u1]) .- view(sol[u2]))
 
 	return sol
 end
@@ -184,4 +181,4 @@ end # module
 
 using GLMakie
 
-Example227_TwoMembranesWithAdhesion.main(Plotter=GLMakie, γ=1e8, θ=0, ϵ=1e-1, N=30, f1 = 25, f2 = -0)
+@time Example227_TwoMembranesWithAdhesion.main(Plotter=GLMakie, γ=1e8, θ=10, ϵ=1, N=30, f1 = 15, f2 = -15)
